6dac1a5e431801f08883f7fdc0811109d1350915
unknown <hasan.rajib1996@gmail.com>
2021-10-18 19:31:21 +0600

    fake data generated

---
+++ src/Pages/Home/Services/Services/Services.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+
+const Services = () => {
+    return (
+        <Row xs={1} md={3} lg={5} className="g-4">
+
+
+        </Row>
+    );
+};
+
+export default Services;
