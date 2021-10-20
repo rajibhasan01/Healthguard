060192a16e518edd23e9b0c3a554bba1ced29588
unknown <hasan.rajib1996@gmail.com>
2021-10-20 11:34:15 +0600

    single Item Details page updated

--- src/Pages/ServicesProvided/SingleServiceDetails/SingleServiceDetails.js
+++ src/Pages/ServicesProvided/SingleServiceDetails/SingleServiceDetails.js
@@ -4,24 +4,22 @@ import { useParams } from 'react-router';
 
 const SingleServiceDetails = () => {
     const { serviceID } = useParams();
-    const [singleItem, setSingleItem] = useState({});
+    const [singleItem, setSingleItem] = useState([]);
 
 
     useEffect(() => {
-        fetch('../../database2.json')
+        fetch('/database2.json')
             .then(res => res.json())
             .then(data => setSingleItem(data))
             .catch(error => { console.log(error.message) })
 
     }, []);
-    console.log(singleItem);
-    let sitem;
-    // for (const item of singleItem) {
-    //     if (serviceID == item.id) {
-    //         sitem = item;
-    //         console.log(sitem);
-    //     }
-    // }
+
+    const key = parseInt(serviceID);
+
+    const displayItem = singleItem.filter(item => key === item.id);
+
+
 
     return (
         <div className="container marginTop py-5">
@@ -29,15 +27,15 @@ const SingleServiceDetails = () => {
 
                 <Card>
                     <h1>loading...</h1>
-                    <Card.Img variant="top" src={sitem?.img} />
+                    <Card.Img variant="top" src={displayItem[0]?.img} />
                     <Card.Body>
-                        <Card.Title>{sitem?.D_name}</Card.Title>
+                        <Card.Title>{displayItem[0]?.D_name}</Card.Title>
                         <Card.Text>
-                            {sitem?.description}
+                            {displayItem[0]?.description}
                         </Card.Text>
                     </Card.Body>
                     <Card.Footer>
-                        <small className="text-muted">Price: ${sitem?.price}</small>
+                        <small className="text-muted">Price: ${displayItem[0]?.price}</small>
                     </Card.Footer>
                 </Card>
             </div>
